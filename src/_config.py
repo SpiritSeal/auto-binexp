b701253ed74d0d08b2635dd4a72d767487634cec
Saketh Reddy <yssaketh@gmail.com>
2024-06-21 13:26:57 -0400

    Cycled api key, moved to .env

--- src/_config.py
+++ src/_config.py
@@ -1,10 +1,14 @@
 from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 
 model = ChatOpenAI(
     # model="claude-3-opus",
     model="oai-gpt-4o",
     base_url="http://bombshell.gtisc.gatech.edu:4000",
-    api_key="sk-G8auP8ht3njROVrqEe6EYQ",
+    api_key=os.getenv("LITELLM_API_KEY"),
     temperature=0,
 )
